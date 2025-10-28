3990df2383a8759f1d9d1474ce5c5eed1218dad6
Julius Truksinas <julius.truksinasinfo@gmail.com>
2025-10-28 21:01:47 +0200

    add transaction model

---
+++ BlockChain/Models/Transaction.cs
@@ -0,0 +1,24 @@
+using BlockChain.Hashers;
+
+namespace BlockChain.Models;
+
+public class Transaction
+{
+    public string TransactionId { get; private set; }
+    public string Sender { get; set; }
+    public string Receiver { get; set; }
+    public decimal Amount { get; set; }
+
+    public Transaction(string sender, string receiver, decimal amount, IHasher hasher)
+    {
+        Sender = sender;
+        Receiver = receiver;
+        Amount = amount;
+        TransactionId = hasher.Hash($"{sender}{receiver}{amount}{Guid.NewGuid()}");
+    }
+
+    public override string ToString()
+    {
+        return $"{TransactionId[..8]}... | {Sender[..6]} -> {Receiver[..6]} | {Amount}";
+    }
+}
